883c0a692fc484d22c4a673ac553b248c9fa182a
JLeVan1 <jlevan1@my.gcu.edu>
2022-03-05 22:56:22 -0700

    Search works

--- src/main/java/com/gcuCLC/controller/ProductController.java
+++ src/main/java/com/gcuCLC/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.gcuCLC.controller;
 
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpSession;
 
@@ -21,194 +22,262 @@ import com.gcuCLC.model.ProductModel;
 import com.gcuCLC.model.User;
 import com.gcuCLC.repository.OrdersRepository;
 
-
-
 @Controller
 @RequestMapping("/productClone")
 public class ProductController {
-	
-	
-//	@Autowired
-//	private EditOrderRepository editRepo;
-	
+
+	// @Autowired
+	// private EditOrderRepository editRepo;
+
 	@Autowired
 	private ProductsBusinessInterface service;
-	
-//	@Autowired
-//	private OrderDataService ods;
-	
+
+	// @Autowired
+	// private OrderDataService ods;
+
 	@Autowired
 	private OrdersRepository orderRepo;
 
-	
-	
 	public static OrderEntity orderEnt;
-	
+
 	@GetMapping("/")
-    public String displayProducts(Model model, User user) {
-		
-        model.addAttribute("title", "Product Form");
-        model.addAttribute("user", new Login().getUsername());
-        model.addAttribute("product", new ProductModel());
-        return "productClone";
-    }
-	
-	
-	
-	
+	public String displayProducts(Model model, User user) {
+
+		model.addAttribute("title", "Product Form");
+		model.addAttribute("user", new Login().getUsername());
+		model.addAttribute("product", new ProductModel());
+		return "productClone";
+	}
+
 	@PostMapping("/doProductClone")
-	public String ProductPage(@ModelAttribute("products") ProductModel productClone, BindingResult bindingResult, Model model) {		
-			
+	public String ProductPage(@ModelAttribute("products") ProductModel productClone, BindingResult bindingResult,
+			Model model) {
+
 		// Check for validation errors
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("title", "Login Form");
 			return "login";
 		}
-		
-		model.addAttribute("Product Form", productClone); 
+
+		model.addAttribute("Product Form", productClone);
 		model.addAttribute("products", service.getProducts());
 		return "products";
 	}
-	
+
 	// View Orders Response
 	@GetMapping("/viewOrders")
-	public String viewOrder(@ModelAttribute Login login, Model model, BindingResult bindingResult)
-	{
-		//System.out.println("/viewOrder Order ID: " + orderEntity.getOrderId() + " " + orderEntity.toString());
-		
-		model.addAttribute("title" , "View All Orders");
+	public String viewOrder(@ModelAttribute Login login, Model model, BindingResult bindingResult) {
+		// System.out.println("/viewOrder Order ID: " + orderEntity.getOrderId() + " " +
+		// orderEntity.toString());
+
+		model.addAttribute("title", "View All Orders");
 		model.addAttribute("orderEntity", new OrderEntity());
-		model.addAttribute("products" , service.getProducts());
+		model.addAttribute("products", service.getProducts());
 		model.addAttribute("login", new Login());
-		//model.addAttribute("viewOrders", orderEntity);
-		
+		// model.addAttribute("viewOrders", orderEntity);
+
 		return "viewOrders";
 	}
-	
+
 	public static Login login = new Login();
-	
+
 	@SuppressWarnings("static-access")
 	@GetMapping("/display")
-	public String loginUser(/*@Valid*/@ModelAttribute Login login, Model model) {		
-		
-		//service.getProducts();
-		this.login  = login;
-		
+	public String loginUser(/* @Valid */@ModelAttribute Login login, Model model) {
+
+		// service.getProducts();
+		this.login = login;
+
 		// Display something
 		model.addAttribute("title", "Login Form");
 		model.addAttribute("products", service.getProducts());
 		model.addAttribute("orderEntity", new OrderEntity());
-		
-		
+
 		return "viewOrders";
 	}
 
-	
+	@RequestMapping(path = { "/search" })
+	public String search(/* @Valid */@ModelAttribute Login login, Model model, String searchTerm) {
+		System.out.println(searchTerm);
+		ArrayList<ProductModel> tempList = new ArrayList<ProductModel>();
+		if (searchTerm.isEmpty())
+			model.addAttribute("products", service.getProducts());
+		else {
+			try {
+				String[] entry = searchTerm.split(":");
+				switch (entry[0]) {
+					case "id":
+						service.getProducts().forEach((p) -> {
+							if (p.getOrderId() == Integer.parseInt(entry[1]))
+								tempList.add(p);
+						});
+						model.addAttribute("products", tempList);
+						break;
+					case "company":
+						service.getProducts().forEach((p) -> {
+							if (p.getCompanyName().equalsIgnoreCase(entry[1]))
+								tempList.add(p);
+						});
+						model.addAttribute("products", tempList);
+						break;
+					case "customer":
+						service.getProducts().forEach((p) -> {
+							if (p.getCustomerName().equalsIgnoreCase(entry[1]))
+								tempList.add(p);
+						});
+						model.addAttribute("products", tempList);
+						break;
+					case "date":
+						service.getProducts().forEach((p) -> {
+							if (p.getDeliveryDate().equalsIgnoreCase(entry[1]))
+								tempList.add(p);
+						});
+						model.addAttribute("products", tempList);
+						break;
+					case "price":
+						service.getProducts().forEach((p) -> {
+							if (p.getDeliveryPrice() == Integer.parseInt(entry[1]))
+								tempList.add(p);
+						});
+						model.addAttribute("products", tempList);
+						break;
+					case "status":
+						service.getProducts().forEach((p) -> {
+							if (p.getDeliveryStatus().equalsIgnoreCase(entry[1]))
+								tempList.add(p);
+						});
+						model.addAttribute("products", tempList);
+						break;
+					case "payment":
+						service.getProducts().forEach((p) -> {
+							if (p.getPayment().equalsIgnoreCase(entry[1]))
+								tempList.add(p);
+						});
+						model.addAttribute("products", tempList);
+						break;
+					default:
+						model.addAttribute("products", new ArrayList<ProductModel>());
+						break;
+				}
+			} catch (Exception e) {
+				model.addAttribute("products", tempList);
+				model.addAttribute("orderEntity", new OrderEntity());
+				return "viewOrders";
+			}
+		}
+		model.addAttribute("orderEntity", new OrderEntity());
+		return "viewOrders";
+	}
+
 	@GetMapping("/editOrder/{orderId}")
-	public String displayFoundId (@RequestParam(required = false) Integer orderId, Integer id, @ModelAttribute("form") OrderEntity form, HttpSession session, Model model, Login login ) {
-		
+	public String displayFoundId(@RequestParam(required = false) Integer orderId, Integer id,
+			@ModelAttribute("form") OrderEntity form, HttpSession session, Model model, Login login) {
+
 		if (form.getOrderId() > 0) {
 		}
 		OrderEntity ue;
 		ue = orderRepo.findByOrderId(form.getOrderId());
 		ProductController.orderEnt = new OrderEntity(ue);
 
-		System.out.println("/editOrder Order ID: " + form.getOrderId() + " " + "Company Name: " + form.getCompanyName() + " " +form.toString() );
+		System.out.println("/editOrder Order ID: " + form.getOrderId() + " " + "Company Name: " + form.getCompanyName()
+				+ " " + form.toString());
 		model.addAttribute("orderEntity", form);
 		model.addAttribute("orderEntity", ProductController.orderEnt);
 		model.addAttribute("login", new Login());
-		
+
 		return "editOrder";
 	}
-	
-	//Mapping for Order Deletion
-		@GetMapping("/removeOrder/{orderId}")
-		public String deleteSingle(@RequestParam(required = false) Integer orderId, Integer id, @ModelAttribute("form") OrderEntity form, HttpSession session, Model model, Login login ) {
-			
-			if (form.getOrderId() > 0) {
-			}
-			OrderEntity ue;
-			ue = orderRepo.findByOrderId(form.getOrderId());
-			ProductController.orderEnt = new OrderEntity(ue);
 
-			System.out.println("/removeOrder Order ID: " + form.getOrderId() + " " + "Company Name: " + form.getCompanyName() + " " +form.toString() );
-			model.addAttribute("orderEntity", form);
-			model.addAttribute("orderEntity", ProductController.orderEnt);
-			model.addAttribute("login", new Login());
-			
-			return "removeOrder";
+	// Mapping for Order Deletion
+	@GetMapping("/removeOrder/{orderId}")
+	public String deleteSingle(@RequestParam(required = false) Integer orderId, Integer id,
+			@ModelAttribute("form") OrderEntity form, HttpSession session, Model model, Login login) {
+
+		if (form.getOrderId() > 0) {
 		}
-		
+		OrderEntity ue;
+		ue = orderRepo.findByOrderId(form.getOrderId());
+		ProductController.orderEnt = new OrderEntity(ue);
+
+		System.out.println("/removeOrder Order ID: " + form.getOrderId() + " " + "Company Name: "
+				+ form.getCompanyName() + " " + form.toString());
+		model.addAttribute("orderEntity", form);
+		model.addAttribute("orderEntity", ProductController.orderEnt);
+		model.addAttribute("login", new Login());
+
+		return "removeOrder";
+	}
+
 	// Update Orders Response
 	@PostMapping("/changeOrder")
-	public String updateOrder(@ModelAttribute Login login, OrderEntity orderEntity, Model model, BindingResult bindingResult, Orders orders )
-	{
-			orderEntity.setOrderId(orderEntity.getOrderId());
-			orderEntity.setCompanyName(orderEntity.getCompanyName());
-			orderEntity.setCustomerName(orderEntity.getCustomerName());
-			orderEntity.setDeliveryDate(orderEntity.getDeliveryDate());
-			orderEntity.setDeliveryPrice(orderEntity.getDeliveryPrice());
-			orderEntity.setDeliveryStatus(orderEntity.getDeliveryStatus());
-			orderEntity.setPayment(orderEntity.getPayment());
-		
+	public String updateOrder(@ModelAttribute Login login, OrderEntity orderEntity, Model model,
+			BindingResult bindingResult, Orders orders) {
+		orderEntity.setOrderId(orderEntity.getOrderId());
+		orderEntity.setCompanyName(orderEntity.getCompanyName());
+		orderEntity.setCustomerName(orderEntity.getCustomerName());
+		orderEntity.setDeliveryDate(orderEntity.getDeliveryDate());
+		orderEntity.setDeliveryPrice(orderEntity.getDeliveryPrice());
+		orderEntity.setDeliveryStatus(orderEntity.getDeliveryStatus());
+		orderEntity.setPayment(orderEntity.getPayment());
+
 		orderRepo.save(orderEntity);
-		
+
 		model.addAttribute("title", "Order Updated");
 		model.addAttribute("orderEntity", new OrderEntity());
 		model.addAttribute("products", service.getProducts());
 		model.addAttribute("editOrder", orderEntity);
 		model.addAttribute("products", orderEntity);
 		model.addAttribute("login", new Login());
-		
-		return viewOrder(login, model, bindingResult); //changed from "viewOrders"
-	}
-	
-	// Update Orders Response
-		@PostMapping("/removeOrder")
-		public String deleteOrder(@ModelAttribute Login login, OrderEntity orderEntity, Model model, BindingResult bindingResult, Orders orders )
-		{
-				orderEntity.setOrderId(orderEntity.getOrderId());
-				orderEntity.setCompanyName(orderEntity.getCompanyName());
-				orderEntity.setCustomerName(orderEntity.getCustomerName());
-				orderEntity.setDeliveryDate(orderEntity.getDeliveryDate());
-				orderEntity.setDeliveryPrice(orderEntity.getDeliveryPrice());
-				orderEntity.setDeliveryStatus(orderEntity.getDeliveryStatus());
-				orderEntity.setPayment(orderEntity.getPayment());
-			
-			orderRepo.delete(orderEntity);
-			
-			model.addAttribute("title", "Order Updated");
-			model.addAttribute("orderEntity", new OrderEntity());
-			model.addAttribute("products", service.getProducts());
-			model.addAttribute("editOrder", orderEntity);
-			model.addAttribute("products", orderEntity);
-			model.addAttribute("login", new Login());
-			
-			return viewOrder(login, model, bindingResult); //changed from "viewOrders"
-		}
-	
-		//Show product viewer
-		@PostMapping("/addOrder")
-		public String addOrder(@ModelAttribute Login login, OrderEntity orderEntity, Model model, BindingResult bindingResult, Orders orders)
-		{
-			System.out.println("/addOrder Order ID: " + orderEntity.getOrderId() + " " + orderEntity.toString() );
-			
-			orderEntity.setCustomerName(orderEntity.getCustomerName());
-			orderEntity.setDeliveryDate(orderEntity.getDeliveryDate());
-			orderEntity.setDeliveryPrice((int)orderEntity.getDeliveryPrice());
-			orderEntity.setDeliveryStatus(orderEntity.getDeliveryStatus());
-			orderEntity.setCompanyName(orderEntity.getCompanyName());
-			orderEntity.setPayment(orderEntity.getPayment());
-			
-			orderRepo.save(orderEntity);
-			
-			model.addAttribute("title", "Order Created");
-			model.addAttribute("orderEntity", new OrderEntity());
-			model.addAttribute("products", service.getProducts());
-			model.addAttribute("products", orderEntity);
-			model.addAttribute("login", new Login());
 
-			return viewOrder(login, model, bindingResult);
-		}
-	
+		return viewOrder(login, model, bindingResult); // changed from "viewOrders"
+	}
+
+	// Update Orders Response
+	@PostMapping("/removeOrder")
+	public String deleteOrder(@ModelAttribute Login login, OrderEntity orderEntity, Model model,
+			BindingResult bindingResult, Orders orders) {
+		orderEntity.setOrderId(orderEntity.getOrderId());
+		orderEntity.setCompanyName(orderEntity.getCompanyName());
+		orderEntity.setCustomerName(orderEntity.getCustomerName());
+		orderEntity.setDeliveryDate(orderEntity.getDeliveryDate());
+		orderEntity.setDeliveryPrice(orderEntity.getDeliveryPrice());
+		orderEntity.setDeliveryStatus(orderEntity.getDeliveryStatus());
+		orderEntity.setPayment(orderEntity.getPayment());
+
+		orderRepo.delete(orderEntity);
+
+		model.addAttribute("title", "Order Updated");
+		model.addAttribute("orderEntity", new OrderEntity());
+		model.addAttribute("products", service.getProducts());
+		model.addAttribute("editOrder", orderEntity);
+		model.addAttribute("products", orderEntity);
+		model.addAttribute("login", new Login());
+
+		return viewOrder(login, model, bindingResult); // changed from "viewOrders"
+	}
+
+	// Show product viewer
+	@PostMapping("/addOrder")
+	public String addOrder(@ModelAttribute Login login, OrderEntity orderEntity, Model model,
+			BindingResult bindingResult, Orders orders) {
+		System.out.println("/addOrder Order ID: " + orderEntity.getOrderId() + " " + orderEntity.toString());
+
+		orderEntity.setCustomerName(orderEntity.getCustomerName());
+		orderEntity.setDeliveryDate(orderEntity.getDeliveryDate());
+		orderEntity.setDeliveryPrice((int) orderEntity.getDeliveryPrice());
+		orderEntity.setDeliveryStatus(orderEntity.getDeliveryStatus());
+		orderEntity.setCompanyName(orderEntity.getCompanyName());
+		orderEntity.setPayment(orderEntity.getPayment());
+
+		orderRepo.save(orderEntity);
+
+		model.addAttribute("title", "Order Created");
+		model.addAttribute("orderEntity", new OrderEntity());
+		model.addAttribute("products", service.getProducts());
+		model.addAttribute("products", orderEntity);
+		model.addAttribute("login", new Login());
+
+		return viewOrder(login, model, bindingResult);
+	}
+
 }
